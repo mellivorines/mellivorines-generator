4258bbcbbaed61e79691eb29b5283d85e46ec251
mzlx015 <68333104+mzlx015@users.noreply.github.com>
2022-10-20 11:20:29 +0800

    Create UserInfoService.java

---
+++ src/main/java/com/allure/generator/service/impl/UserInfoService.java
@@ -0,0 +1,54 @@
+
+package com.allure.generator.service.impl;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.allure.generator.repository.dao.IUserInfoDao;
+import com.allure.generator.repository.entity.UserInfoEntity;
+import com.allure.generator.service.IUserInfoService;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * 功能描述:
+ *
+ * @author lWX1117952
+ * @since 2022-10-19
+ */
+@Service
+@AllArgsConstructor
+public class UserInfoService implements IUserInfoService {
+
+    private final IUserInfoDao userInfoDao;
+
+    /**
+     * 获取所有用户信息
+     *
+     * @return 返回结果
+     */
+    @Override
+    public List<UserInfoEntity> getAllUserInfo() {
+        List<UserInfoEntity> list = userInfoDao.list();
+        return list;
+    }
+
+    /**
+     * 添加用户
+     *
+     * @param userInfoEntity 用户信息
+     * @return 返回是否成功
+     */
+    @Override
+    public String saveUserInfo(UserInfoEntity userInfoEntity) {
+
+        userInfoEntity.setCreator("lilinxi");
+        userInfoEntity.setUpdater("lilinxi");
+
+        boolean save = userInfoDao.save(userInfoEntity);
+        return String.valueOf(save);
+    }
+}
