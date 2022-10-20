8f1aa82a83b630747ce8d01c07baf2b1ffb8cd14
mzlx015 <68333104+mzlx015@users.noreply.github.com>
2022-10-20 11:24:45 +0800

    Create BaseEntity.java

---
+++ src/main/java/com/allure/generator/repository/entity/BaseEntity.java
@@ -0,0 +1,53 @@
+
+package com.allure.generator.repository.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * 功能描述:
+ *
+ * @author lWX1117952
+ * @since 2022-10-19
+ */
+@Data
+@ApiModel("实体映射基类")
+public class BaseEntity implements Serializable {
+
+    /**
+     * 创建者
+     */
+    @ApiModelProperty("创建者")
+    private String creator;
+
+    /**
+     * 更新者
+     */
+    @ApiModelProperty("更新者")
+    private String updater;
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @ApiModelProperty("创建时间")
+    private LocalDateTime createTime;
+
+    /**
+     * 更新时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @ApiModelProperty("更新时间")
+    private LocalDateTime updateTime;
+
+}
