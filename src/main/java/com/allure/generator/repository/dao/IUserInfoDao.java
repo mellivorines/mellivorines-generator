a44188d1825616c0f15cee0c545636b8e7caf7ca
mzlx015 <68333104+mzlx015@users.noreply.github.com>
2022-10-20 11:25:45 +0800

    Create IUserInfoDao.java

---
+++ src/main/java/com/allure/generator/repository/dao/IUserInfoDao.java
@@ -0,0 +1,14 @@
+
+package com.allure.generator.repository.dao;
+
+import com.allure.generator.repository.entity.UserInfoEntity;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * 功能描述:
+ *
+ * @author lWX1117952
+ * @since 2022-10-19
+ */
+public interface IUserInfoDao extends IService<UserInfoEntity> {
+}
