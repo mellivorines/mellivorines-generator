2013af2ed498e6dbf08e2d9085ead9292c0cc8b3
mzlx015 <68333104+mzlx015@users.noreply.github.com>
2022-10-20 11:25:11 +0800

    Create UserInfoEntity.java

---
+++ src/main/java/com/allure/generator/repository/entity/UserInfoEntity.java
@@ -0,0 +1,42 @@
+
+package com.allure.generator.repository.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 功能描述:
+ *
+ * @author lWX1117952
+ * @since 2022-10-19
+ */
+@Data
+@TableName("allure_user_info")
+@EqualsAndHashCode(callSuper = true)
+@ApiModel("用户实体映射类")
+public class UserInfoEntity extends BaseEntity {
+    /**
+     * 主键
+     */
+    @ApiModelProperty("id")
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    @ApiModelProperty("用户名")
+    private String userName;
+
+    /**
+     * 密码
+     */
+    @ApiModelProperty("密码")
+    private String password;
+}
