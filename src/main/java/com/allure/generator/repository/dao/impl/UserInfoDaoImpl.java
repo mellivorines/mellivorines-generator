eeff9ec1e107ec64bf3ff0f9f2cf4d042db98c1b
mzlx015 <68333104+mzlx015@users.noreply.github.com>
2022-10-20 11:26:22 +0800

    Create UserInfoDaoImpl.java

---
+++ src/main/java/com/allure/generator/repository/dao/impl/UserInfoDaoImpl.java
@@ -0,0 +1,19 @@
+
+package com.allure.generator.repository.dao.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.allure.generator.repository.dao.IUserInfoDao;
+import com.allure.generator.repository.entity.UserInfoEntity;
+import com.allure.generator.repository.mapper.UserInfoMapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+/**
+ * 功能描述:
+ *
+ * @author lWX1117952
+ * @since 2022-10-19
+ */
+@Service
+public class UserInfoDaoImpl extends ServiceImpl<UserInfoMapper, UserInfoEntity> implements IUserInfoDao {
+}
