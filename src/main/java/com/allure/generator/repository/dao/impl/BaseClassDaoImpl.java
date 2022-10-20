5460349824a9cd3a0158a84c7c826fc7ad25a281
mzlx015 <68333104+mzlx015@users.noreply.github.com>
2022-10-20 15:34:26 +0800

    Create BaseClassDaoImpl.java

---
+++ src/main/java/com/allure/generator/repository/dao/impl/BaseClassDaoImpl.java
@@ -0,0 +1,19 @@
+
+package com.allure.generator.repository.dao.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.allure.generator.repository.dao.IBaseClassDao;
+import com.allure.generator.repository.entity.BaseClassPO;
+import com.allure.generator.repository.mapper.BaseClassMapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+/**
+ * 功能描述:实体基类相关
+ *
+ * @author lWX1117952
+ * @since 2022-10-20
+ */
+@Service
+public class BaseClassDaoImpl extends ServiceImpl<BaseClassMapper, BaseClassPO> implements IBaseClassDao {
+}
