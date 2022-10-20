ee1c29dfd8a686d0a600e4c401f0dcdee7364f07
mzlx015 <68333104+mzlx015@users.noreply.github.com>
2022-10-20 16:45:50 +0800

    Update AllureCodeGeneratorApplication.java

--- src/main/java/com/allure/generator/AllureCodeGeneratorApplication.java
+++ src/main/java/com/allure/generator/AllureCodeGeneratorApplication.java
@@ -7,6 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 
+/**
+ * 功能描述:程序启动类
+ *
+ * @author lilinxi
+ * @since 2022-10-19
+ */
 @SpringBootApplication
 @EnableKnife4j
 @MapperScan("com.allure.generator.repository.mapper")
