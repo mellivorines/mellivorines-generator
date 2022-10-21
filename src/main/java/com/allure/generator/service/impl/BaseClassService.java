a3e8ed215980a2df0b5b57edebf8fe2dfc38a94b
mzlx015 <68333104+mzlx015@users.noreply.github.com>
2022-10-21 14:10:11 +0800

    Update BaseClassService.java

--- src/main/java/com/allure/generator/service/impl/BaseClassService.java
+++ src/main/java/com/allure/generator/service/impl/BaseClassService.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.allure.generator.model.BaseClassVO;
 import com.allure.generator.repository.dao.IBaseClassDao;
 import com.allure.generator.repository.entity.BaseClassPO;
 import com.allure.generator.service.IBaseClassService;
 
+import cn.hutool.core.bean.BeanUtil;
 import lombok.AllArgsConstructor;
 
 /**
@@ -29,24 +31,46 @@ public class BaseClassService implements IBaseClassService {
      * @return 返回结果
      */
     @Override
-    public List<BaseClassPO> getAll() {
-
-        return iBaseClassDao.list();
+    public List<BaseClassVO> getAll() {
+        return BeanUtil.copyToList(iBaseClassDao.list(), BaseClassVO.class);
     }
 
     /**
      * 保存实体基类
      *
-     * @param baseClassPO 实体基类数据
+     * @param baseClassVO 实体基类数据
      * @return 返回是否保存成功
      */
     @Override
-    public String save(BaseClassPO baseClassPO) {
-
+    public String saveBaseClass(BaseClassVO baseClassVO) {
+        BaseClassPO baseClassPO = BeanUtil.copyProperties(baseClassVO, BaseClassPO.class);
         baseClassPO.setCreator("lilinxi");
         baseClassPO.setUpdater("lilinxi");
-
         boolean save = iBaseClassDao.save(baseClassPO);
         return String.valueOf(save);
+    }
+
+    /**
+     * 更新实体基类
+     *
+     * @param baseClassVO 实体基类数据
+     * @return 返回是否更新成功
+     */
+    @Override
+    public String updateBaseClass(BaseClassVO baseClassVO) {
+        BaseClassPO baseClassPO = BeanUtil.copyProperties(baseClassVO, BaseClassPO.class);
+        baseClassPO.setUpdater("lilinxi");
+        return String.valueOf(iBaseClassDao.updateById(baseClassPO));
+    }
+
+    /**
+     * 删除实体基类
+     *
+     * @param ids 主键id
+     * @return 返回是否删除成功
+     */
+    @Override
+    public String deleteBaseClass(List<Integer> ids) {
+        return String.valueOf(iBaseClassDao.removeBatchByIds(ids));
     }
 }
