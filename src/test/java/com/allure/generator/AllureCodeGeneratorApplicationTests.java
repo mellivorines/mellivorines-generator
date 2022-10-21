b7034d81118755634e908f6b8a8a2080b0a98c58
mzlx015 <68333104+mzlx015@users.noreply.github.com>
2022-10-21 14:13:13 +0800

    test init

--- src/test/java/com/allure/generator/AllureCodeGeneratorApplicationTests.java
+++ src/test/java/com/allure/generator/AllureCodeGeneratorApplicationTests.java
@@ -3,7 +3,12 @@ package com.allure.generator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+/**
+ * 功能描述:测试程序启动类
+ *
+ * @author lilinxi
+ * @since 2022-10-19
+ */
 @SpringBootTest
 class AllureCodeGeneratorApplicationTests {
 
