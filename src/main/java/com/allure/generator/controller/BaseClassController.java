a33505b4c4fde2bccb652494aff8067c5fe670ec
mzlx015 <68333104+mzlx015@users.noreply.github.com>
2022-10-21 14:11:35 +0800

    Update BaseClassController.java

--- src/main/java/com/allure/generator/controller/BaseClassController.java
+++ src/main/java/com/allure/generator/controller/BaseClassController.java
@@ -1,17 +1,27 @@
 
 package com.allure.generator.controller;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.allure.generator.constants.Result;
 import com.allure.generator.controller.base.BaseController;
-import com.allure.generator.repository.entity.BaseClassPO;
+import com.allure.generator.model.BaseClassVO;
 import com.allure.generator.service.IBaseClassService;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * 功能描述:实体基类相关
@@ -28,14 +38,26 @@ public class BaseClassController extends BaseController {
     IBaseClassService baseClassService;
 
     @GetMapping(value = "/list")
-    @ApiOperation("所有用户")
-    public Result<List<BaseClassPO>> getList() {
+    @ApiOperation("所有基类")
+    public Result<List<BaseClassVO>> getList() {
         return process(result -> baseClassService.getAll());
     }
 
-    @PostMapping(value = "/login")
-    @ApiOperation("添加用户")
-    public Result getBuLevelResult(@RequestBody BaseClassPO baseClassPO) {
-        return process(result -> baseClassService.save(baseClassPO));
+    @PostMapping(value = "/save")
+    @ApiOperation("添加基类")
+    public Result saveBaseClass(@Valid @RequestBody BaseClassVO baseClassVO) {
+        return process(result -> baseClassService.saveBaseClass(baseClassVO));
+    }
+
+    @PutMapping(value = "/update")
+    @ApiOperation("更新基类")
+    public Result updateBaseClass(@Valid @RequestBody BaseClassVO baseClassVO) {
+        return process(result -> baseClassService.updateBaseClass(baseClassVO));
+    }
+
+    @DeleteMapping(value = "/delete")
+    @ApiOperation("添加基类")
+    public Result deleteBaseClass(@Valid @RequestBody List<Integer> ids) {
+        return process(result -> baseClassService.deleteBaseClass(ids));
     }
 }
