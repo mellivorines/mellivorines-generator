0ceff0e0c6398de36f9df23a22afbbfa2162f1de
mzlx015 <68333104+mzlx015@users.noreply.github.com>
2022-11-01 15:57:27 +0800

    Update UserInfoController.java

--- src/main/java/com/allure/generator/controller/UserInfoController.java
+++ src/main/java/com/allure/generator/controller/UserInfoController.java
@@ -1,63 +1,45 @@
 
 package com.allure.generator.controller;
 
-import java.util.List;
-
-import javax.annotation.Resource;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.allure.generator.constants.Result;
-import com.allure.generator.controller.base.BaseController;
-import com.allure.generator.model.UserInfoVO;
-import com.allure.generator.service.IUserInfoService;
-
+import com.allure.generator.dao.AllureUserInfoDao;
+import com.allure.generator.entity.AllureUserInfo;
+import com.allure.generator.entity.AllureUserInfoFetcher;
+import com.allure.generator.entity.AllureUserInfoTable;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import lombok.AllArgsConstructor;
+import org.babyfish.jimmer.sql.JSqlClient;
+import org.babyfish.jimmer.sql.fluent.Fluent;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-/**
- * 功能描述:用户信息相关
- *
- * @author lilinxi
- * @since 2022-10-19
- */
+import javax.annotation.Resource;
+import java.util.List;
+
+
 @RestController
-@AllArgsConstructor
 @Api(tags = "用户信息处理")
 @RequestMapping("api/user-info")
-public class UserInfoController extends BaseController {
+public class UserInfoController {
 
     @Resource
-    IUserInfoService userInfoService;
+    AllureUserInfoDao allureUserInfoDao;
 
-    @GetMapping(value = "/list")
+    @Resource
+    private JSqlClient sqlClient;
+
+    @PostMapping(value = "/list")
     @ApiOperation("所有用户")
-    public Result<List<UserInfoVO>> getList() {
-        return process(result -> userInfoService.getAllUserInfo());
+    @ResponseBody
+    public List<AllureUserInfo> getList() {
+        Fluent fluent = sqlClient.createFluent();
+        AllureUserInfoTable allureUserInfoTable = new AllureUserInfoTable();
+        List<AllureUserInfo> execute = fluent.query(allureUserInfoTable)
+                .groupBy(allureUserInfoTable.id())
+                .select(allureUserInfoTable.fetch(AllureUserInfoFetcher.$.userName().password()))
+                .execute();
+        return execute;
     }
 
-    @PostMapping(value = "/save")
-    @ApiOperation("添加用户")
-    public Result saveUserInfo(@RequestBody UserInfoVO userInfoVO) {
-        return process(result -> userInfoService.saveUserInfo(userInfoVO));
-    }
-
-    @PutMapping(value = "/update")
-    @ApiOperation("更新用户")
-    public Result updateUserInfo(@RequestBody UserInfoVO userInfoVO) {
-        return process(result -> userInfoService.updateUserInfo(userInfoVO));
-    }
-
-    @DeleteMapping(value = "/delete")
-    @ApiOperation("删除用户")
-    public Result deleteUserInfo(@RequestBody List<Integer> ids) {
-        return process(result -> userInfoService.deleteUserInfo(ids));
-    }
 }
