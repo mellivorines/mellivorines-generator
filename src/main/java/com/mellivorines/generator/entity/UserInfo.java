cdc8026a238208e4fd1710587b4b1bb78a45a45b
mellivorines <68333104+mellivorines@users.noreply.github.com>
2022-11-02 17:01:52 +0800

    Create UserInfo.java

---
+++ src/main/java/com/mellivorines/generator/entity/UserInfo.java
@@ -0,0 +1,23 @@
+package com.mellivorines.generator.entity;
+
+import org.babyfish.jimmer.sql.*;
+import org.jetbrains.annotations.Nullable;
+
+
+@Entity
+@Table(name = "user_info")
+public interface UserInfo extends UserInfoBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    int id();
+
+    @Nullable
+    @Column(name = "user_name")
+    String userName();
+
+    @Nullable
+    String password();
+
+}
