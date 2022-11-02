8e5a9bdda708ad87b3e1a854824a044e451a4533
mellivorines <68333104+mellivorines@users.noreply.github.com>
2022-11-02 16:57:12 +0800

    Create JacksonConfig.java

---
+++ src/main/java/com/mellivorines/generator/config/JacksonConfig.java
@@ -0,0 +1,13 @@
+package com.mellivorines.generator.config;
+
+import org.babyfish.jimmer.jackson.ImmutableModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ImmutableModule immutableModule() {
+        return new ImmutableModule();
+    }
+}
