61d9f1197a00a6ecde73a83a3166a7f573edf4ee
lilinxi <lilinxi015@163.com>
2022-11-02 21:06:07 +0800

    change dev-jimmer-gradle

--- src/main/java/com/mellivorines/generator/GeneratorApplication.java
+++ src/main/java/com/mellivorines/generator/GeneratorApplication.java
@@ -5,12 +5,6 @@ import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * 功能描述:程序启动类
- *
- * @author lilinxi
- * @since 2022-10-19
- */
 @SpringBootApplication
 @EnableKnife4j
 public class GeneratorApplication {
--- src/main/java/com/mellivorines/generator/dao/UserInfoDao.java
+++ src/main/java/com/mellivorines/generator/dao/UserInfoDao.java
@@ -17,7 +17,6 @@ public class UserInfoDao extends BaseDao<UserInfoTable, UserInfo> {
 
     @Override
     public List<UserInfo> findAllByPage(Class<UserInfoTable> entityTableClazz, int page, int size) {
-        List<UserInfo> allureUserInfos = sqlClient.createQuery(entityTableClazz, RootSelectable<UserInfoTable>::select).limit(size, page * size).execute();
         return sqlClient.createQuery(entityTableClazz, RootSelectable<UserInfoTable>::select).limit(size, page * size).execute();
     }
 
