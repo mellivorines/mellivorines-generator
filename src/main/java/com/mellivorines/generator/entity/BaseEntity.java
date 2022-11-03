904efde3100aa2007831b1482b8e02fc612464ba
mellivorines <68333104+mellivorines@users.noreply.github.com>
2022-11-03 19:46:51 +0800

    Create BaseEntity.java

---
+++ src/main/java/com/mellivorines/generator/entity/BaseEntity.java
@@ -0,0 +1,36 @@
+package com.mellivorines.generator.entity;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.babyfish.jimmer.sql.Column;
+import org.babyfish.jimmer.sql.MappedSuperclass;
+import org.jetbrains.annotations.Nullable;
+
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 自动生成的base接口，用于写关联关系，每次生成不会覆盖此文件。
+ * </p>
+ *
+ * @Date 2022-10-28 09:15:07
+ */
+@MappedSuperclass
+public interface BaseEntity {
+
+
+    @Nullable
+    String creator();
+
+    @Nullable
+    String updater();
+
+    @Nullable
+    @Column(name = "create_time")
+    LocalDateTime createTime();
+
+    @Nullable
+    @Column(name = "update_time")
+    LocalDateTime updateTime();
+
+}
