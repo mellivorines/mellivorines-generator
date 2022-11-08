011900d83caa3ab36d6472666be92ca7c02e3cd0
mellivorines <68333104+mellivorines@users.noreply.github.com>
2022-11-08 16:06:04 +0800

    Create ProjectModify.java

---
+++ src/main/java/com/mellivorines/generator/entity/ProjectModify.java
@@ -0,0 +1,49 @@
+package com.mellivorines.generator.entity;
+
+import org.babyfish.jimmer.sql.Column;
+import org.babyfish.jimmer.sql.Entity;
+import org.babyfish.jimmer.sql.Table;
+import org.jetbrains.annotations.Nullable;
+
+@Entity
+@Table(name = "gen_project_modify")
+public interface ProjectModify extends BaseEntity {
+
+    @Nullable
+    @Column(name = "project_name")
+    String projectName();
+
+    @Nullable
+    @Column(name = "project_code")
+    String projectCode();
+
+    @Nullable
+    @Column(name = "project_package")
+    String projectPackage();
+
+
+    @Nullable
+    @Column(name = "project_path")
+    String projectPath();
+
+    @Nullable
+    @Column(name = "modify_project_name")
+    String modifyProjectName();
+
+    @Nullable
+    @Column(name = "modify_project_code")
+    String modifyProjectCode();
+
+    @Nullable
+    @Column(name = "exclusions")
+    String exclusions();
+
+    @Nullable
+    @Column(name = "modify_suffix")
+    String modifySuffix();
+
+    @Nullable
+    @Column(name = "modify_tmp_path")
+    String modifyTmpPath();
+
+}
