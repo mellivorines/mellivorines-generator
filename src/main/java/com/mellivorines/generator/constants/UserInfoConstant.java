7008af9eb9ee491f44214aee2734855573fc9de2
mellivorines <68333104+mellivorines@users.noreply.github.com>
2022-11-14 11:06:31 +0800

    Create UserInfoConstant.java

---
+++ src/main/java/com/mellivorines/generator/constants/UserInfoConstant.java
@@ -0,0 +1,12 @@
+package com.mellivorines.generator.constants;
+
+public interface UserInfoConstant {
+
+    String AUTHOR = "mellivorines";
+
+    String EMAIL = "lilinxi015@163.com";
+
+    String URL = "";
+
+    String TITLE = "代码生成器";
+}
