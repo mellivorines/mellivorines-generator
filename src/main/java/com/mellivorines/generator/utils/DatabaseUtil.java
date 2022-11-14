1717dd19c35dc500823ecb3b3c47bb25c3cfc1b3
mellivorines <68333104+mellivorines@users.noreply.github.com>
2022-11-14 11:04:43 +0800

    Create DatabaseUtil.java

---
+++ src/main/java/com/mellivorines/generator/utils/DatabaseUtil.java
@@ -0,0 +1,24 @@
+package com.mellivorines.generator.utils;
+
+import com.mellivorines.generator.config.GenDataSource;
+import com.mellivorines.generator.constants.DbType;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseUtil {
+    private static final int CONNECTION_TIMEOUTS_SECONDS = 6;
+
+    public static Connection getConnection(GenDataSource dataSource) throws ClassNotFoundException, SQLException {
+        DriverManager.setLoginTimeout(CONNECTION_TIMEOUTS_SECONDS);
+        Class.forName(dataSource.getDbType().getDriverClass());
+
+        Connection connection = DriverManager.getConnection(dataSource.getConnUrl(), dataSource.getUsername(), dataSource.getPassword());
+//        if (dataSource.getDbType() == DbType.Oracle) {
+//            ((OracleConnection) connection).setRemarksReporting(true);
+//        }
+
+        return connection;
+    }
+}
