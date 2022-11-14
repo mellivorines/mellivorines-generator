c30f649775f8e96c05bfeb38161e119c8140d173
mellivorines <68333104+mellivorines@users.noreply.github.com>
2022-11-14 11:07:06 +0800

    Update CommonConstant.java

--- src/main/java/com/mellivorines/generator/constants/CommonConstant.java
+++ src/main/java/com/mellivorines/generator/constants/CommonConstant.java
@@ -4,12 +4,8 @@ public interface CommonConstant {
 
     String SLASH = "/";
 
-    String TEMPLATE_FILE_BASE_PATH="src/main/resources/template";
+    String TEMPLATE_FILE_BASE_PATH = "src/main/resources/template";
 
-    interface TemplateType {
-
-        String FILE_TYPE = ".ftl";
-    }
 
     interface GeneratorType {
 
