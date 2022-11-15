a7a54425d13977ad901d431d88d657173d660a5c
mellivorines <68333104+mellivorines@users.noreply.github.com>
2022-11-15 17:40:42 +0800

    Update GeneratorController.java

--- src/main/java/com/mellivorines/generator/controller/GeneratorController.java
+++ src/main/java/com/mellivorines/generator/controller/GeneratorController.java
@@ -1,8 +1,6 @@
 package com.mellivorines.generator.controller;
 
 import com.mellivorines.generator.constants.CommonConstant;
-import com.mellivorines.generator.dao.ProjectModifyDao;
-import com.mellivorines.generator.entity.ProjectModify;
 import com.mellivorines.generator.utils.FreemarkerUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,22 +17,19 @@ import java.util.Map;
 @RequestMapping("code/generator")
 public class GeneratorController {
 
-    @Resource
-    ProjectModifyDao projectModifyDao;
 
     @GetMapping(value = "/module")
     @ApiOperation("生成模块")
     @ResponseBody
     public void generateModule() throws Exception {
 
-        ProjectModify projectModify = projectModifyDao.findById(ProjectModify.class, 1).get();
 
         String templateFilePath = CommonConstant.TEMPLATE_FILE_BASE_PATH + CommonConstant.SLASH + CommonConstant.GeneratorType.MODULE;
         String templateFileName = "Controller.java";
         String outFileClassPath = "src/main";
         Map<String, Object> dataModel = new HashMap<>();
-        dataModel.put("package", projectModify.projectPackage());
-        dataModel.put("module", projectModify.modifyProjectCode());
+        dataModel.put("package", "");
+        dataModel.put("module", "");
         dataModel.put("ClassName", "baseClass");
         dataModel.put("isSwagger", false);
 
