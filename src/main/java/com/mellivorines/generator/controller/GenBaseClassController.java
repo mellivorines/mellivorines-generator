55e10c55f69a95cac50a4e6598ea3a89f6603781
mellivorines <68333104+mellivorines@users.noreply.github.com>
2022-11-15 17:41:10 +0800

    Create GenBaseClassController.java

---
+++ src/main/java/com/mellivorines/generator/controller/GenBaseClassController.java
@@ -0,0 +1,57 @@
+package com.mellivorines.generator.controller;
+
+
+import com.mellivorines.generator.dao.BaseClassDao;
+import com.mellivorines.generator.entity.GenBaseClass;
+import com.mellivorines.generator.entity.GenBaseClassTable;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.babyfish.jimmer.sql.ast.mutation.DeleteResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@Api(tags = "基类处理")
+@RequestMapping("api/base-class")
+public class GenBaseClassController {
+    @Resource
+    BaseClassDao baseClassDao;
+
+    @GetMapping(value = "/list")
+    @ApiOperation("所有基类")
+    @ResponseBody
+    public List<GenBaseClass> getList(@RequestParam("page") Integer page, @RequestParam("size") Integer size) {
+        return baseClassDao.findAllByPage(GenBaseClassTable.class, page, size);
+    }
+
+    @PostMapping(value = "/save")
+    @ApiOperation("保存基类")
+    @ResponseBody
+    public GenBaseClass save(@RequestBody GenBaseClass genBaseClass) {
+        return baseClassDao.save(genBaseClass);
+    }
+
+    @PutMapping(value = "/update")
+    @ApiOperation("更新基类")
+    @ResponseBody
+    public GenBaseClass update(@RequestBody GenBaseClass genBaseClass) {
+        return baseClassDao.update(genBaseClass);
+    }
+
+    @DeleteMapping(value = "/delete")
+    @ApiOperation("删除基类")
+    @ResponseBody
+    public DeleteResult delete(@RequestBody List<Integer> ids) {
+        return baseClassDao.batchDelete(GenBaseClassTable.class, ids);
+    }
+}
