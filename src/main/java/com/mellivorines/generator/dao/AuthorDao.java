26a1747ad171e50166353a9a8a7b83285195309b
mellivorines <68333104+mellivorines@users.noreply.github.com>
2022-11-15 17:47:16 +0800

    Create AuthorDao.java

---
+++ src/main/java/com/mellivorines/generator/dao/AuthorDao.java
@@ -0,0 +1,25 @@
+package com.mellivorines.generator.dao;
+
+import com.mellivorines.generator.entity.GenAuthor;
+import com.mellivorines.generator.entity.GenAuthorFetcher;
+import com.mellivorines.generator.entity.GenAuthorTable;
+import org.babyfish.jimmer.sql.JSqlClient;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+public class AuthorDao extends BaseDao<GenAuthorTable, GenAuthor>{
+
+    @Resource
+    private JSqlClient sqlClient;
+    @Override
+    public List<GenAuthor> findAllByPage(Class<GenAuthorTable> entityTableClazz, int page, int size) {
+        GenAuthorTable genAuthorTable = new GenAuthorTable();
+        return sqlClient.createQuery(genAuthorTable)
+                .groupBy(genAuthorTable.id())
+                .select(genAuthorTable.fetch(GenAuthorFetcher.$.allScalarFields()))
+                .limit(size, (page - 1) * size)
+                .execute();
+    }
+
+}
