65d6592069995ecedab81599591e595e9092c674
mellivorines <68333104+mellivorines@users.noreply.github.com>
2022-11-15 17:46:51 +0800

    Update BaseClassDao.java

--- src/main/java/com/mellivorines/generator/dao/BaseClassDao.java
+++ src/main/java/com/mellivorines/generator/dao/BaseClassDao.java
@@ -1,8 +1,8 @@
 package com.mellivorines.generator.dao;
 
-import com.mellivorines.generator.entity.BaseClass;
-import com.mellivorines.generator.entity.BaseClassFetcher;
-import com.mellivorines.generator.entity.BaseClassTable;
+import com.mellivorines.generator.entity.GenBaseClass;
+import com.mellivorines.generator.entity.GenBaseClassFetcher;
+import com.mellivorines.generator.entity.GenBaseClassTable;
 import org.babyfish.jimmer.sql.JSqlClient;
 import org.springframework.stereotype.Repository;
 
@@ -10,16 +10,16 @@ import javax.annotation.Resource;
 import java.util.List;
 
 @Repository
-public class BaseClassDao extends BaseDao<BaseClassTable, BaseClass> {
+public class BaseClassDao extends BaseDao<GenBaseClassTable, GenBaseClass> {
     @Resource
     private JSqlClient sqlClient;
 
     @Override
-    public List<BaseClass> findAllByPage(Class<BaseClassTable> entityTableClazz, int page, int size) {
-        BaseClassTable baseClassTable = new BaseClassTable();
-        return sqlClient.createQuery(baseClassTable)
-                .groupBy(baseClassTable.id())
-                .select(baseClassTable.fetch(BaseClassFetcher.$.packageName().code().fields().remark()))
+    public List<GenBaseClass> findAllByPage(Class<GenBaseClassTable> entityTableClazz, int page, int size) {
+        GenBaseClassTable genBaseClassTable = new GenBaseClassTable();
+        return sqlClient.createQuery(genBaseClassTable)
+                .groupBy(genBaseClassTable.id())
+                .select(genBaseClassTable.fetch(GenBaseClassFetcher.$.allScalarFields()))
                 .limit(size, (page - 1) * size)
                 .execute();
     }
