6f5b94d56bfe86dc2c8f5b97d2a8a88506f84649
mellivorines <68333104+mellivorines@users.noreply.github.com>
2022-11-17 15:21:20 +0800

    Create TemplateConstant.java

---
+++ src/main/java/com/mellivorines/generator/constants/TemplateConstant.java
@@ -0,0 +1,51 @@
+package com.mellivorines.generator.constants;
+
+public interface TemplateConstant {
+
+    /**
+     * 模板类型：支持ftl和vm
+     */
+    String FILE_TYPE_FTL = ".ftl";
+
+    String FILE_TYPE_VM = ".vm";
+
+
+    String SWAGGER = "swagger";
+
+    /**
+     * 包名
+     */
+    String PACKAGE = "package";
+
+    /**
+     * 微服务专用模块
+     */
+    String MODULE = "module";
+
+    /**
+     * 项目名称
+     */
+    String PROJECT = "project";
+
+
+    /**
+     * 版本3件套
+     */
+    String GROUP = "group";
+
+    String ARTIFACT = "artifact";
+
+    String VERSION = "version";
+
+
+    /**
+     * 作者相关
+     */
+    String AUTHOR = "author";
+
+    String EMAIL = "email";
+
+    String URL = "url";
+
+    String TELEPHONE = "telephone";
+}
